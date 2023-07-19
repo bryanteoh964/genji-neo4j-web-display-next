670e9d639d0554036d8cd3cec90881a7122e2580
Yu Zeng <zengy2003@gmail.com>
2023-07-18 20:14:34 -0700

    mid stage poem commit

--- src/app/poems/[chapter]/[poem]/page.jsx
+++ src/app/poems/[chapter]/[poem]/page.jsx
@@ -2,6 +2,14 @@
 
 import { usePathname,  useSearchParams} from 'next/navigation'
 import {useRouter} from 'next/navigation'
+
+
+import { useMemo, useState, useReducer, useEffect } from 'react'
+import { Button, Col, Divider, Input, Row, Space, Select, Tag, } from 'antd';
+// import 'antd/dist/antd.min.css';
+import TextArea from 'antd/lib/input/TextArea';
+import { Link } from 'react-router-dom';
+
 const PoemPage = (params) => {
   //const router = usePathname();
 
@@ -9,7 +17,30 @@ const PoemPage = (params) => {
   
   let chapter = params.params.chapter
   let poem = params.params.poem
-  
+
+  const [speaker, setSpeaker] = useState([])
+  const [addressee, setAddressee] = useState([])
+  // Japanese and Romaji
+  const [JPRM, setJPRM] = useState([])
+  const [trans, setTrans] = useState({
+      Waley: 'N/A',
+      Seidensticker: 'N/A',
+      Tyler: 'N/A',
+      Washburn: 'N/A',
+      Cranston: 'N/A'
+  })
+  const [source, setSource] = useState([]) // currently linked honka
+  const [rel, setRel] = useState([]) // currently linked related poems
+  const [IA, setIA] = useState('') // internal allusion selection
+  const [pnum, setPnum] = useState([])
+  const [query, setQuery] = useState([])
+  const [tag, setTag] = useState([]) // currently linked tags
+  const [tagType, setTagType] = useState([''])
+  const [select, setSelect] = useState('')
+  const [notes, setNotes] = useState("")
+  const [auth, setAuth] = useState(false)
+  const [usr, setUsr] = useState('')
+  const [pwd, setPwd] = useState('')
 
   return (
     <div>
