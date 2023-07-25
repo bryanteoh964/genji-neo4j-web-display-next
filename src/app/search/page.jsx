17e3032393bcbecf8a94848750caea57939e7291
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-07-24 20:46:11 -0400

    Search page CSS

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -2,9 +2,11 @@
 
 import PoemSearch from '@/components/PoemSearch';
 
+import styles from "../../styles/pages/search.module.css"
+
 const page = () => {
   return (
-    <div>
+    <div className={styles.viewport_container}>
 	    <PoemSearch />
     </div>
   )
