a1e1099f009572b24c88c629baf3a1fc5e8d4106
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-07-27 17:27:16 -0400

    Formatting for js files

--- src/app/api/allusions/query/route.js
+++ src/app/api/allusions/query/route.js
@@ -1,17 +1,15 @@
 const { getSession } = require('../neo4j_driver/route.js');
 
 export const GET = async (request )=> {
-  try{
-    
-    const session = await getSession();
+	try{    
+		const session = await getSession();
+		const{searchParams} = new URL(request.url)
+		const key = searchParams.get('key')
+		let write = await session.writeTransaction(tx => tx.run('MATCH (h:Honka {id: $key})<-[:TRANSLATION_OF]-(t:Translation)<-[:TRANSLATOR_OF]-(p:People {name:$selectedTranslation}) SET h.Honka = $honka, h.Romaji = $romaji, h.notes = $notes, t.translation = $translation return "OK"', {key: key, selectedTranslation: selectedTranslation, honka: honka, romaji: romaji, notes: notes, translation: translation}))
 
-    const{searchParams} = new URL(request.url)
-    const key = searchParams.get('key')
-    let write = await session.writeTransaction(tx => tx.run('MATCH (h:Honka {id: $key})<-[:TRANSLATION_OF]-(t:Translation)<-[:TRANSLATOR_OF]-(p:People {name:$selectedTranslation}) SET h.Honka = $honka, h.Romaji = $romaji, h.notes = $notes, t.translation = $translation return "OK"', {key: key, selectedTranslation: selectedTranslation, honka: honka, romaji: romaji, notes: notes, translation: translation}))
-    
-    console.log('sucess',write)
-    return new Response(JSON.stringify(write), {status: 200})
-  }catch(error){
-    return new Response(error,{status:500})
-  }
+		console.log('sucess',write)
+		return new Response(JSON.stringify(write), {status: 200})
+	}catch(error){
+		return new Response(error,{status:500})
+	}
 }
