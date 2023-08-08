894c769f106d5dbc39ed476c2be65f2c2ee880f1
Yu Zeng <zengy2003@gmail.com>
2023-08-08 11:15:05 -0700

    textblock in progress

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,37 +1,34 @@
-'use client'
+// components/TextBlock.js
 
-
-
-function TextBlock({ text, searchTerm, data }) {
-  const height = Math.min(text.length, 500); 
-  const weidth = text.length  // Adjust max value as needed
-  const blockSize = height /text.length;
+function TextBlock({ text, searchTerm,data}) {
+  const height = Math.min(text.length, 500);  // Adjust max value as needed
   
-  // Generate an array to count the number of occurrences at each character index
-  const charOccurrence = new Array(text.length).fill(0);
-  Object.keys(data).forEach(word => {
-    data[word].forEach(index => {
-      for (let i = 0; i < word.length; i++) {
-        charOccurrence[index + i]++;
-      }
-    });
-  });
-
+  const occurrences = data[searchTerm] || [];
+  const blockSize = height / text.length;
+  const blocksPerRow = 5; 
   return (
-    <div style={{ height: `${height}px`, width: '500px', background: 'lightgray', position: 'relative' }}>
-      {charOccurrence.map((occurrence, index) => (
-        <div
-          key={index}
-          style={{
-            height: `${blockSize * searchTerm.length}px`,
-            width: '50px',
-            background: occurrence > 0 ? `rgba(255, 0, 0, ${Math.min(occurrence / 10, 1)})` : 'transparent',
-            position: 'absolute',
-            bottom: `${index * blockSize}px`
-          }}
-        ></div>
-      ))}
-    </div>
+     // Adjust this to how many blocks you want in a row
+
+<div style={{ height: `${height}px`, width: `${50 * blocksPerRow}px`, background: 'lightgray', position: 'relative' }}>
+  {occurrences.map((_, index) => {
+    const row = Math.floor(index / blocksPerRow);
+    const col = index % blocksPerRow;
+    
+    return (
+      <div
+        key={index}
+        style={{
+          height: `${blockSize * searchTerm.length}px`,  // Adjust for word length
+          width: '50px',
+          background: 'red',
+          position: 'absolute',
+          bottom: `${(blockSize * searchTerm.length + 5) * row}px`,  // Add 5 for spacing in height
+          left: `${(50 + 5) * col}px`  // 50px is the width of each block + 5px spacing
+        }}
+      ></div>
+    );
+  })}
+</div>
   );
 }
 
