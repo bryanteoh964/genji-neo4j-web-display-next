ff585721029f6e263ef49e3221cbc7021c639350
Yu Zeng <zengy2003@gmail.com>
2023-08-09 13:27:31 -0700

    whole data set

--- src/app/api/micro_search/route.js
+++ src/app/api/micro_search/route.js
@@ -2,9 +2,9 @@ import data from '../micro_search/data'
 
 export const GET = async (request)=> {
 	try{
-    
+    console.log('in progress')
 		const write = data;
-   
+    
 		return new Response(JSON.stringify(write), {status: 200})
 	}catch(error){
 		return new Response(error,{status:500})
