16bd6f9c3bf46d70797505652d92298839c632e6
Yu Zeng <zengy2003@gmail.com>
2023-08-08 17:15:47 -0700

    WOrd search

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,34 +1,50 @@
-// components/TextBlock.js
+function TextBlock({ text, searchTerm, data }) {
+  const maxDisplayWidth = 800;  // Desired maximum width of the display area
+  const maxDisplayHeight = 30;  // Desired maximum height of the display area
+  const dotSize = 5;  // Diameter of each dot
 
-function TextBlock({ text, searchTerm,data}) {
-  const height = Math.min(text.length, 500);  // Adjust max value as needed
-  
   const occurrences = data[searchTerm] || [];
-  const blockSize = height / text.length;
-  const blocksPerRow = 5; 
-  return (
-     // Adjust this to how many blocks you want in a row
+  const textLength = 10000;
 
-<div style={{ height: `${height}px`, width: `${50 * blocksPerRow}px`, background: 'lightgray', position: 'relative' }}>
-  {occurrences.map((_, index) => {
-    const row = Math.floor(index / blocksPerRow);
-    const col = index % blocksPerRow;
-    
-    return (
-      <div
-        key={index}
-        style={{
-          height: `${blockSize * searchTerm.length}px`,  // Adjust for word length
-          width: '50px',
-          background: 'red',
-          position: 'absolute',
-          bottom: `${(blockSize * searchTerm.length + 5) * row}px`,  // Add 5 for spacing in height
-          left: `${(50 + 5) * col}px`  // 50px is the width of each block + 5px spacing
-        }}
-      ></div>
-    );
-  })}
-</div>
+  const dotsPerRow = Math.floor(maxDisplayWidth / (dotSize + 1));
+  const totalRows = Math.ceil(textLength / dotsPerRow);
+  const potentialHeight = totalRows * (dotSize + 1);
+  const displayHeight = Math.min(potentialHeight, maxDisplayHeight);
+
+  const getColor = (position) => {
+      // Count overlaps by filtering occurrences within a close range
+      const closeOccurrences = occurrences.filter(
+          pos => Math.abs(pos - position) < (textLength / displayHeight) * dotSize
+      ).length;
+      const opacity = Math.min(closeOccurrences / 5, 1);  // Adjust divisor for desired brightness scaling
+      return `rgba(255, 0, 0, ${opacity})`;
+  };
+
+  return (
+      <div style={{ height: `${displayHeight}px`, width: `${maxDisplayWidth}px`, position: 'relative' }}>
+          {occurrences.map((position, index) => {
+              const relativePosition = (position / textLength) * displayHeight;
+
+              // Calculate row and col based on the position in the text
+              const row = Math.floor(position / dotsPerRow);
+              const col = position % dotsPerRow;
+
+              return (
+                  <div
+                      key={index}
+                      style={{
+                          height: `${dotSize}px`,
+                          width: `${dotSize}px`,
+                          background: getColor(position),
+                          borderRadius: '50%',  // Makes the shape a circle
+                          position: 'absolute',
+                          top: `${row * (dotSize + 1)}px`,
+                          left: `${col * (dotSize + 1)}px`
+                      }}
+                  ></div>
+              );
+          })}
+      </div>
   );
 }
 
