fc6151f623e79bb141f006e18bfa4c4e28bad674
Yu Zeng <zengy2003@gmail.com>
2023-08-20 16:50:14 -0700

    multiple word color mircro search

--- src/components/TextBlock.prod.jsx
+++ src/components/TextBlock.prod.jsx
@@ -6,29 +6,29 @@ function TextBlock({ searchTerms, data }) {
     const grayRowHeight = 10;
     const whiteRowHeight = 5;
     const totalRowHeight = grayRowHeight + whiteRowHeight;
+    const colors = ["255, 0, 0", "0, 128, 128", "0, 0, 255", "128, 0, 128", "255, 165, 0"]; // RGB values of colors
 
     const maxDotsInWidth = Math.floor(maxDisplayWidth / dotSize);
     const maxDotsInHeight = Math.floor(maxDisplayHeight / totalRowHeight);
     const centeringOffset = (grayRowHeight - dotSize) / 2;
 
-    const allOccurrences = searchTerms.flatMap(term => data[term] || []);
-
-    const normalizePosition = (position) => {
-        const normalizedRow = Math.floor(position / maxDotsInWidth) % maxDotsInHeight;
-        const normalizedCol = position % maxDotsInWidth;
-        return { row: normalizedRow, col: normalizedCol };
-    };
-
     const frequencyMap = {};
-    allOccurrences.forEach((position) => {
-        const { row, col } = normalizePosition(position);
-        const key = `${row}-${col}`;
-        frequencyMap[key] = (frequencyMap[key] || 0) + 1;
+    const colorMap = {};
+
+    searchTerms.forEach((term, index) => {
+        const occurrences = data[term] || [];
+        occurrences.forEach((position) => {
+            const normalizedRow = Math.floor(position / maxDotsInWidth) % maxDotsInHeight;
+            const normalizedCol = position % maxDotsInWidth;
+            const key = `${normalizedRow}-${normalizedCol}`;
+            frequencyMap[key] = (frequencyMap[key] || 0) + 1;
+            colorMap[key] = colors[index % colors.length];
+        });
     });
 
-    const getColor = (count) => {
+    const getColor = (count, color) => {
         let opacity = Math.min(1, minOpacity + Math.log(count + 1) / 10);
-        return `rgba(255, 0, 0, ${opacity})`;
+        return `rgba(${color}, ${opacity})`;
     };
 
     return (
@@ -51,13 +51,14 @@ function TextBlock({ searchTerms, data }) {
             {Object.keys(frequencyMap).map((key, index) => {
                 const [row, col] = key.split('-').map(Number);
                 const count = frequencyMap[key];
+                const color = colorMap[key];
                 return (
                     <div
                         key={index}
                         style={{
                             height: `${dotSize}px`,
                             width: `${dotSize}px`,
-                            background: getColor(count),
+                            background: getColor(count, color),
                             borderRadius: '50%',
                             position: 'absolute',
                             top: `${row * totalRowHeight + centeringOffset}px`,
