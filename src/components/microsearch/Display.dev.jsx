8ffcca7d006b978cb9dbcbf65d68f5539df73d74
Yu Zeng <zengy2003@gmail.com>
2023-12-10 15:47:42 -0500

    displaying sentence in progress

--- src/components/microsearch/Display.dev.jsx
+++ src/components/microsearch/Display.dev.jsx
@@ -1,12 +1,16 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect,useState } from 'react';
 import * as d3 from 'd3';
 
 const SimpleHeatmap = () => {
   const svgRef = useRef(null);
+  const gridSize = 50; // Adjust grid size for the larger dataset
+  const totalWords = 580000; // Total number of words in the dataset
+  const [blockIndicesApproach2, setBlockIndicesApproach2] = useState(Array.from({ length: gridSize * gridSize }, () => []));
+
 
   useEffect(() => {
     const fetchData = async () => {
-      const wordToTrack = 'fall'; // Word to track in the dataset
+      const wordToTrack = 'winter'; // Word to track in the dataset
 
       const response = await fetch(`/api/micro_search`);
       const wordIndices = await response.json();
@@ -16,8 +20,7 @@ const SimpleHeatmap = () => {
       if (wordToTrack.toLowerCase() in wordIndices) {
         console.log(`${wordToTrack} exists in the data.`); // Log if the word to track exists
 
-        const gridSize = 100; // Adjust grid size for the larger dataset
-        const totalWords = 580000; // Total number of words in the dataset
+
 
         const occurrences = Array(gridSize * gridSize).fill(0);
 
@@ -28,13 +31,19 @@ const SimpleHeatmap = () => {
   .fill(null)
   .map(() => []);
         // Calculate occurrences for each word index and assign to grid blocks
-        wordIndicesArray.forEach(([wordIndex,sentenceIndex]) => {
-          const blockIndex = Math.floor((wordIndex / totalWords) * (gridSize * gridSize));
-          occurrences[blockIndex]++;
-          blockIndices[blockIndex].push(sentenceIndex);
-          console.log("hey",wordIndex,sentenceIndex)
+        wordIndicesArray.forEach(([wordIndex, sentenceIndex]) => {
+          const blockIndexApproach1 = Math.floor((wordIndex / totalWords) * (gridSize * gridSize));
+    
+          const blockX = Math.floor(((wordIndex % totalWords) / totalWords) * gridSize);
+          const blockY = Math.floor((wordIndex / totalWords) * gridSize);
+          const blockIndexApproach2 = blockY * gridSize + blockX;
+    
+          occurrences[blockIndexApproach1]++;
+          blockIndices[blockIndexApproach1].push(sentenceIndex);
+    
+          blockIndicesApproach2[blockIndexApproach2].push([wordIndex, sentenceIndex]);
+          console.log("xixi",blockIndicesApproach2)
         });
-
         const margin = { top: 10, right: 20, bottom: 30, left: 30 };
         const width = 375 - margin.left - margin.right; // Adjusted width for the heatmap
         const height = 550 - margin.top - margin.bottom; // Adjusted height for the heatmap
@@ -65,21 +74,20 @@ const SimpleHeatmap = () => {
           .attr('width', xScale.bandwidth())
           .attr('height', yScale.bandwidth())
           .style('fill', d => colorScale(d))
-          .on('click', (event) => {
+          .on('click', (event, d, i) => {
             const mouseX = event.offsetX; // X-coordinate of the mouse click relative to the SVG
             const mouseY = event.offsetY; // Y-coordinate of the mouse click relative to the SVG
         
-            // Calculate the block index based on the mouse position
+            // Calculate the block index based on the mouse position for Approach 2
             const blockX = Math.floor((mouseX / width) * gridSize);
             const blockY = Math.floor((mouseY / height) * gridSize);
-            const blockIndex = blockY * gridSize + blockX;
-        
-            // Access associated wordIndex and sentenceIndex values based on blockIndex
-            const associatedIndices = blockIndices[blockIndex];
+            const blockIndexApproach2 = blockY * gridSize + blockX;
+    
+            // Access associated wordIndex and sentenceIndex values based on blockIndex for Approach 2
+            const associatedIndices = blockIndicesApproach2[blockIndexApproach2];
         
             // Perform actions with associatedIndices here
-            console.log('Rectangle clicked!', blockIndex, associatedIndices);
-
+            console.log('Rectangle clicked!', blockIndexApproach2, associatedIndices);
           });
       } else {
         console.error(`${wordToTrack} not found in the data.`);
@@ -93,3 +101,5 @@ const SimpleHeatmap = () => {
 };
 
 export default SimpleHeatmap;
+
+
