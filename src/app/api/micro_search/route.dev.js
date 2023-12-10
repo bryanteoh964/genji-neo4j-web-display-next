e5acc778918f602a7e27aea911ad351dc5451ffa
Yu Zeng <zengy2003@gmail.com>
2023-12-09 21:44:29 -0500

    displaying correctly

--- src/app/api/micro_search/route.dev.js
+++ src/app/api/micro_search/route.dev.js
@@ -3,7 +3,7 @@ import data from './waley_indexes.json'
 export const GET = async (request) => {
 	try{
 		const write = data;
-    
+    console.log()
 		return new Response(JSON.stringify(write), {status: 200})
 	} catch(error){
 		return new Response(error,{status:500})
--- src/components/microsearch/Display.dev.jsx
+++ src/components/microsearch/Display.dev.jsx
@@ -6,7 +6,7 @@ const SimpleHeatmap = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const wordToTrack = 'at'; // Word to track in the dataset
+      const wordToTrack = 'fall'; // Word to track in the dataset
 
       const response = await fetch(`/api/micro_search`);
       const wordIndices = await response.json();
@@ -17,28 +17,27 @@ const SimpleHeatmap = () => {
         console.log(`${wordToTrack} exists in the data.`); // Log if the word to track exists
 
         const gridSize = 100; // Adjust grid size for the larger dataset
+        const totalWords = 580000; // Total number of words in the dataset
 
-        const data = Array(gridSize * gridSize).fill(0).map((_, index) => {
-          const x = index % gridSize;
-          const y = Math.floor(index / gridSize);
+        const occurrences = Array(gridSize * gridSize).fill(0);
 
-          const blockStart = Math.floor((wordIndices[wordToTrack.toLowerCase()][0][0] * index) / (gridSize * gridSize));
-          const blockEnd = Math.floor((wordIndices[wordToTrack.toLowerCase()][0][0] * (index + 1)) / (gridSize * gridSize));
+        const wordIndicesArray = wordIndices[wordToTrack.toLowerCase()];
+        console.log("hey",wordIndicesArray)
 
-          const occurrences = (wordIndices[wordToTrack.toLowerCase()] || []).filter(
-            ([wordIndex]) => wordIndex >= blockStart && wordIndex < blockEnd
-          ).length;
-
-          return {
-            x,
-            y,
-            value: occurrences,
-          };
+        const blockIndices = Array(gridSize * gridSize)
+  .fill(null)
+  .map(() => []);
+        // Calculate occurrences for each word index and assign to grid blocks
+        wordIndicesArray.forEach(([wordIndex,sentenceIndex]) => {
+          const blockIndex = Math.floor((wordIndex / totalWords) * (gridSize * gridSize));
+          occurrences[blockIndex]++;
+          blockIndices[blockIndex].push(sentenceIndex);
+          console.log("hey",wordIndex,sentenceIndex)
         });
 
         const margin = { top: 10, right: 20, bottom: 30, left: 30 };
-        const width = 350 - margin.left - margin.right;
-        const height = 550 - margin.top - margin.bottom;
+        const width = 375 - margin.left - margin.right; // Adjusted width for the heatmap
+        const height = 550 - margin.top - margin.bottom; // Adjusted height for the heatmap
 
         const svg = d3.select(svgRef.current)
           .attr('width', width + margin.left + margin.right)
@@ -55,17 +54,33 @@ const SimpleHeatmap = () => {
           .range([0, height]);
 
         const colorScale = d3.scaleSequential(d3.interpolateYlOrRd)
-          .domain([0, 580000 / (gridSize * gridSize)]); // Values based on word occurrences
+          .domain([0, Math.max(...occurrences)]); // Values based on word occurrences
 
         svg.selectAll()
-          .data(data)
+          .data(occurrences)
           .enter()
           .append('rect')
-          .attr('x', d => xScale(d.x))
-          .attr('y', d => yScale(d.y))
+          .attr('x', (_, i) => xScale(i % gridSize))
+          .attr('y', (_, i) => yScale(Math.floor(i / gridSize)))
           .attr('width', xScale.bandwidth())
           .attr('height', yScale.bandwidth())
-          .style('fill', d => colorScale(d.value));
+          .style('fill', d => colorScale(d))
+          .on('click', (event) => {
+            const mouseX = event.offsetX; // X-coordinate of the mouse click relative to the SVG
+            const mouseY = event.offsetY; // Y-coordinate of the mouse click relative to the SVG
+        
+            // Calculate the block index based on the mouse position
+            const blockX = Math.floor((mouseX / width) * gridSize);
+            const blockY = Math.floor((mouseY / height) * gridSize);
+            const blockIndex = blockY * gridSize + blockX;
+        
+            // Access associated wordIndex and sentenceIndex values based on blockIndex
+            const associatedIndices = blockIndices[blockIndex];
+        
+            // Perform actions with associatedIndices here
+            console.log('Rectangle clicked!', blockIndex, associatedIndices);
+
+          });
       } else {
         console.error(`${wordToTrack} not found in the data.`);
       }
