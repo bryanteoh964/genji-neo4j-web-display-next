470be5490451387f53cc447a9c24ca488790a3c7
Yu Zeng <zengy2003@gmail.com>
2024-01-14 02:07:20 -0800

    sentence showing

--- src/components/microsearch/MicroSearchReader1.dev.jsx
+++ src/components/microsearch/MicroSearchReader1.dev.jsx
@@ -4,6 +4,7 @@ import { ThingsContext } from './context.dev.js';
 
 const Reader1 = () => {
   const [sentenceIndex, setSentenceIndex] = useState(0);
+  const [data, setData] = useState ([])
   const { value2, setValue2 } = useContext(ThingsContext);
   useEffect(() => {
     setSentenceIndex(value2)
@@ -15,15 +16,18 @@ const Reader1 = () => {
   //use a usestate to store it for effcience for sure ***
   const fetchData = async () => {
   
-    // const response = await fetch(`/api/micro_search/sentences`);
-    // const senIndices = await response.json();
-    // console.log('sentence Indices:', senIndices); // Log the word indices to check the API response
+    const response = await fetch(`/api/micro_search/sentences`);
+     const senIndices = await response.json();
+     setData(senIndices)
+    console.log('sentence Indices:', senIndices); // Log the word indices to check the API response
 
   };
 
     return (
         <div >
+
           <h3>Sentence Index: {sentenceIndex}</h3>
+          <p>data: {data[sentenceIndex+2]}</p>
         </div>
     )
 }
