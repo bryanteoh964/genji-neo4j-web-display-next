1bd31d194ebe8576ac3b695c57de67196efe14be
Shen Liu <slsteven@bu.edu>
2024-09-15 21:20:24 -0400

    fixed character page, about content might overflow; added @media in css for character page, poem page, nav bar, heading

--- src/app/poems/[chapter]/[poem]/page.prod.jsx
+++ src/app/poems/[chapter]/[poem]/page.prod.jsx
@@ -7,14 +7,13 @@ import styles from "../../../../styles/pages/poems.module.css"
 const PoemPage = (params) => {
 
     return (
-        <div className={styles.search_display_container}>
+
             <PoemQueryResults
                 poemData={{ 
                     chapterNum: params.params.chapter, 
                     poemNum: params.params.poem 
                 }}
             />
-        </div>
     )
 }
 
