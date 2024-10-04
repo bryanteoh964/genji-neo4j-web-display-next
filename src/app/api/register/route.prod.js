0bbc144e6e02d19817d2469628558fdbcceba918
Shen Liu <slsteven@bu.edu>
2024-10-04 02:21:59 -0400

    created register page for test; register api is connected and can inster data into mongo db

---
+++ src/app/api/register/route.prod.js
@@ -0,0 +1,31 @@
+import bcrypt from 'bcryptjs';
+import client from '../../../lib/db.prod';
+import { NextResponse } from 'next/server'
+
+export async function POST(req) {
+    try {
+        const {email, password} = await req.json()
+
+        const db = await client.db("user");
+        const collection = db.collection("info");
+
+        const existingUser = await collection.findOne({email});
+
+        if (existingUser) {
+            return NextResponse.json({ message: "Email already exists" }, { status: 400 });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await collection.insertOne({
+            email,
+            password: hashedPassword,
+            createdAt: new Date()
+        })
+
+        return NextResponse.json({ message: "User created", userEmail: result.email }, { status: 201 });
+    } catch (error) {
+        console.error("registration has error:", error);
+        return NextResponse.json({ message: "registration api error"}, { status: 500 });
+    }
+}
