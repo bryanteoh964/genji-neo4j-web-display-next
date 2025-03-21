369f266fecd692b0aca8b4ec31cbe0ebb3180363
Shen Liu <slsteven@bu.edu>
2025-03-21 13:50:36 -0400

    added comments, contribution, and fav poem with pagination on user homepage

--- src/app/api/user/getComments/route.prod.js
+++ src/app/api/user/getComments/route.prod.js
@@ -7,35 +7,64 @@ import { ObjectId } from 'mongodb';
 export async function GET(req) {
     const session = await auth();
 
+    let page = 1;
+    let limit = 5;
+
     if (!session) {
         return NextResponse.json({ message: 'Unauthorized'}, { status: 401 });
     }
 
-    const { searchParams } = new URL(req.url);
-    const userId = searchParams.get('userId');
-
     try {
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get('userId');
+        const pageParam = searchParams.get('page');
+        const limitParam = searchParams.get('limit');
+
+        if (pageParam) {
+            const parsedPage = parseInt(pageParam);
+            if (!isNaN(parsedPage) && parsedPage > 0) {
+              page = parsedPage;
+            }
+        }
+          
+          if (limitParam) {
+            const parsedLimit = parseInt(limitParam);
+            if (!isNaN(parsedLimit) && parsedLimit > 0) {
+              limit = parsedLimit;
+            }
+        }
+
+        const skip = (page - 1) * limit;
+    
         const db = await client.db('user');
 
         const comments = await db.collection('discussion')
-            .find({ 
-                user: userId,
-                isHidden: false
-            })
-            .sort({ createdAt: -1 })
+            .aggregate([
+                { 
+                    $match: {
+                        user: userId,
+                        isHidden: false
+                    }
+                },
+                {
+                    $sort: {  
+                        createdAt: -1   
+                    }
+                }
+            ])
+            .skip(skip)
+            .limit(limit)
             .toArray();
 
         // console.log('userId:', userId);
 
+        if (!comments || comments.length === 0) {
+            return NextResponse.json({ comments: [] }, { status: 200 });
+        }
 
-        if (!comments) {
-            return NextResponse.json({ 
-                    message: 'No comments found for user'
-                },{ status: 404 }
-            );
-        };
+        const totalComments = await db.collection('discussion').countDocuments({ user: userId, isHidden: false });
 
-        return NextResponse.json({ comments }, { status: 200 });
+        return NextResponse.json({ comments, totalComments, currentPage: page, totalPages: Math.ceil(totalComments / limit) || 1 }, { status: 200 });
 
     } catch (error) {
         console.error('Error finding comments:', error);
