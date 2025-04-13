e0d35cac2e6d9535552091156287ea27d6c194d6
jhuang2026 <j.huang7000@gmail.com>
2025-04-12 23:42:52 -0400

    Removed Microsearch + Other CSS

--- src/app/characters/page.prod.jsx
+++ src/app/characters/page.prod.jsx
@@ -1,10 +1,10 @@
 'use client';
 import { useParams } from 'next/navigation';
-import CharacterDetail from '../../components/CharacterBaseProfile.prod'; // Adjust if necessary
+import CharactersListPage from '../../components/CharacterBaseProfile.prod'; // Adjust if necessary
 
 export default function CharacterPage() {
 	const { name } = useParams();
 
 	// Passing the decoded name to the CharacterDetail component
-	return <CharacterDetail name={decodeURIComponent(name)} />;
+	return <CharactersListPage name={decodeURIComponent(name)} />;
 }
--- src/components/Edit.dev.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Edit = () => {
-  return (
-    <div>Temporary Edit Placeholder</div>
-  )
-}
-
-export default Edit
